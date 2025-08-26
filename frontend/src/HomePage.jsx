9e3efe2a8c3665b962f70a58426163db2e4db62e
Raghavan <727723euit177@skcet.ac.in>
2025-08-26 14:38:44 +0530

    Added Footer Section

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './CSSFolder/home.css';
 import NavBar from "./HomePageComponents/NavBar.jsx";
 import Hero from "./HomePageComponents/Hero.jsx";
+import FeaturedDoctors from "./HomePageComponents/FeaturedDoctors.jsx";
+import Footer from "./HomePageComponents/Footer.jsx";
 
 const HomePage = () => {
     return (
         <>
         <NavBar/>
         <Hero/>
+            <FeaturedDoctors/>
+            <Footer/>
         </>
 
     );
